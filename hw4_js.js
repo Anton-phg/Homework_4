b229b620a6928a560286aef457f07c000cae18d9
Anton-phg <warriorato@gmail.com>
2025-04-28 08:41:15 +0000

    Homework 4, I did not make TASK 4

---
+++ hw4_js.js
@@ -0,0 +1,66 @@
+// TASK 1+
+
+// let width;
+// let height;
+
+// function calcRectangleArea(){  
+//     return width*height;
+// }
+
+// function enterNumbers(){
+//     width = prompt ('Введіть ширину', 4);
+//     height = prompt ('Введіть висоту', 5);
+//     if (width||height == Nan){
+//        alert ("Некоректне значення");
+//     } 
+// }
+// enterNumbers();
+
+// TASK2+
+
+// let age = prompt('Введіть вік', '');
+// function checkAge(){
+// if (age > 14 && age < 127) {
+//     alert( 'Ласкаво просимо!' );
+//   } else if (age == '' || age== null){
+//     alert ('“The field is empty! Please enter your age”');
+//     console.log(`Не введено значення`);
+//   } else if (age <=14 && age > 0) {
+//     alert( 'Вам ще рано' );
+//     console.log(`Вік ${age} не відповідає параметрам входу`)
+//   }  else if (age <0 || age>127){
+//     alert( 'Так не буває' );
+//     console.log(`Значення ${age} не відповідає параметрам входу`)
+//   } else {
+//     alert( 'Невірне значення' );
+//     console.log(`Значення '${age}' є нечисловим`)
+//   }
+// }
+
+// checkAge();
+
+// TASK3 +- РЕФЛІЗОВАНО БЕЗ КЛАСІВ
+
+// let MonthException = [ 'Січень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень', 'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень'];
+// let Month;
+// function showMonthName(){
+//     Month = prompt ('Введіть номер місяця', 2);
+//     if (Month > 0 && Month <=12){
+//         alert (MonthException[Month - 1]);
+//     } else {
+//         alert ('Невірне значення')
+//     }
+// }
+
+// showMonthName();
+
+
+// TASK4-
+
+
+
+
+
+
+
+
